7746dfd92a3f5c1201d695a0091c818304ca7804
Aras Firat <arasfirat124@gmail.com>
2024-04-11 23:15:34 +0300

    JsonDataManager.java +deleteFile()
    saveBookToJson is now working as intended

--- src/main/java/com/example/book_catalog_system/JsonDataManager.java
+++ src/main/java/com/example/book_catalog_system/JsonDataManager.java
@@ -1,46 +1,16 @@
 package com.example.book_catalog_system;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.*;
-import java.util.List;
+
 
 public class JsonDataManager {
 
-    // Method to save a list of books to a JSON file
-    // TODO : WRITE TO A JSON FİLE
-
-    // TODO : REMOVE JSON FILE
-    public static void saveBooksToJson(List<BookManager.Book> books, String filename) {
-        JSONArray jsonArray = new JSONArray();
-        for (BookManager.Book book : books) {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("isbn", book.getIsbn());
-            jsonObject.put("title", book.getTitle());
-            jsonObject.put("subtitle", book.getSubtitle());
-            jsonObject.put("author", book.getAuthor());
-            jsonObject.put("translator", book.getTranslator());
-            jsonObject.put("publisher", book.getPublisher());
-            jsonObject.put("date", book.getDate());
-            jsonObject.put("edition", book.getEdition());
-            jsonObject.put("tag", book.getTag());
-            jsonObject.put("rating", book.getRating());
-            jsonObject.put("cover", book.getCover());
-            jsonArray.put(jsonObject);
-        }
+    // Saves the information of the book which is passed as a parameter on to a json file.
+    public static void saveBookToJson(BookManager.Book book, String filename) {
         try (Writer writer = new FileWriter(filename)) {
-            jsonArray.write(writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Method to append a single book to an existing JSON file
-    public static void appendBookToJson(BookManager.Book book, String filename) {
-        try (Reader reader = new FileReader(filename)) {
-            JSONArray jsonArray = new JSONArray(new JSONTokener(reader));
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("isbn", book.getIsbn());
             jsonObject.put("title", book.getTitle());
@@ -53,21 +23,29 @@ public class JsonDataManager {
             jsonObject.put("tag", book.getTag());
             jsonObject.put("rating", book.getRating());
             jsonObject.put("cover", book.getCover());
-            jsonArray.put(jsonObject);
-            try (Writer writer = new FileWriter(filename)) {
-                jsonArray.write(writer);
-            }
+
+            writer.write(jsonObject.toString());
+
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("There is no such file. Save action failed.");
+
+        }
+    }
+    //Simply deletes the given file
+    public static void deleteFile(String filename){
+        File myObj = new File(filename);
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
         }
     }
 
-    // Method to read books from a JSON file
+    // Reads attributes from a json and returns a book object with said attributes.
     public static BookManager.Book readBooksFromJson(String filename) {
-        //TODO: Get the book's ISBN and add ".json" to the end of it. This is your filename.
         try (Reader reader = new FileReader(filename)) {
-            JSONArray jsonArray = new JSONArray(new JSONTokener(reader));
-            JSONObject jsonObject = jsonArray.getJSONObject(0); // Assuming there's only one book in the JSON array
+            JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
             BookManager.Book book = new BookManager.Book();
             book.setAuthor(jsonObject.getString("author"));
             book.setIsbn(jsonObject.getString("isbn"));
@@ -83,29 +61,26 @@ public class JsonDataManager {
             return book;
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("There is no such file. Read action failed.");
         }
         return null;
     }
 
-    // Sample usage
+
     public static void main(String[] args) {
-        // Sample usage
-        List<BookManager.Book> books = List.of(
-                new BookManager.Book("1234567890", "Book Title", "Subtitle", "Author",
-                        "Translator", "Publisher", "2024-04-10", "First Edition",
-                        "Tag", "Rating", "Cover Image URL")
-        );
+        //initializing a book to use in the functions
+        BookManager.Book aras = new BookManager.Book("1234567890", "zo", "Subtitle", "Author",
+                "Translator", "Publisher", "za", "First Edition",
+                "Tag", "Rating", "Cover Image URL");
 
-        // Save books to JSON file
-        saveBooksToJson(books, "aras.json");
 
-        // Append a book to the JSON file
-        appendBookToJson(new BookManager.Book("0987654321", "Another Book", "", "Author 2",
-                "", "Publisher 2", "2024-04-11", "Second Edition",
-                "Tag", "Rating", "Cover Image URL"), "aras.json");
+        // save test
+        saveBookToJson(aras, "aras.json");
 
-        // Read books from JSON file
-        //BookManager.Book book = readBooksFromJson("books.json");
-        System.out.println(books.get(0));
+        // read test
+        BookManager.Book book = readBooksFromJson("aras.json");
+        // reads correctly as seen
+        System.out.println(book.getAuthor());
+        System.out.println(book.getTitle());
     }
 }
